0eadd0f357ce8873daa8401c73ef746f3e53fbc7
IvanBrasilico <brasilico.ivan@gmail.com>
2018-02-22 14:00:52 -0300

    wsgi sentry

--- wsgi_sentinela.py
+++ wsgi_sentinela.py
@@ -6,7 +6,8 @@ from ajna_commons.flask.log import error_handler, sentry_handler
 if __name__ == '__main__':
     print('Iniciando Servidor Bhadrasana...')
     app.logger.addHandler(error_handler)
-    app.logger.addHandler(sentry_handler)
+    if sentry_handler:
+        app.logger.addHandler(sentry_handler)
     app.logger.setLevel(logging.WARNING)
     app.logger.warning('Servidor (re)iniciado!')
     # To log more things, change the level:
